cbd617f50d398d0cc86945493d2d039492cd0399
maxwelloniate <maxwelloniate@gmail.com>
2020-01-30 11:03:30 -0500

    Added side nav and music to assets folder.

--- dist/index.php
+++ dist/index.php
@@ -15,10 +15,44 @@ $userLoggedIn = $_SESSION["userLoggedIn"];
 <?php require('includes/header.php'); ?>
 
 <section id="main">
+
   <div class="top-container">
+
+    <!-- SIDE NAVBAR -->
+    <nav id="navbar">
+      <a href="index.php" class="logo">
+        <i class="fas fa-adjust"></i>
+        Maximus
+      </a>
+
+      <a href="search.php" class="navItemLink">
+        <div class="search">
+          Search
+          <span><i class="fas fa-search"></i></span>
+        </div>
+      </a>
+
+      <ul class="list-group">
+        <li>
+          <a href="browse.php" class="navItemLink">Browse</a>
+        </li>
+        <li>
+          <a href="your-music.php" class="navItemLink">Your Music</a>
+        </li>
+        <li>
+          <a href="profile.php" class="profile">Profile</a>
+        </li>
+      </ul>
+
+
 
   </div>
 
+  </nav>
+
+  </div>
+
+  <!-- BOTTOM PLAY BAR -->
   <div id="play-bar-container" class="fixed-bottom">
     <div class="play-bar">
       <div class="row">
@@ -105,6 +139,7 @@ $userLoggedIn = $_SESSION["userLoggedIn"];
       </div>
     </div>
   </div>
+
 </section>
 
 
