ef707e39ca3676067ef64a4de4ee2de36f9f6815
maxwelloniate <maxwelloniate@gmail.com>
2020-02-12 12:11:02 -0500

    Removed isset.

--- dist/ajax/updateUserDetails.php
+++ dist/ajax/updateUserDetails.php
@@ -5,27 +5,26 @@ require('../includes/classes/Account.php');
 require('../includes/classes/FormSanitizer.php');
 require('../includes/classes/Constants.php');
 
-if (isset($_POST["username"])) {
-  $account = new Account($con);
 
-  $username = $_POST['username'];
-  $firstName = FormSanitizer::sanitizeFormString($_POST["firstName"]);
-  $lastName = FormSanitizer::sanitizeFormString($_POST["lastName"]);
-  $email = FormSanitizer::sanitizeFormEmail($_POST["email"]);
+$account = new Account($con);
 
-  if ($account->updateDetails($firstName, $lastName, $email, $username)) {
-    echo "
+$username = $_POST['username'];
+$firstName = FormSanitizer::sanitizeFormString($_POST["firstName"]);
+$lastName = FormSanitizer::sanitizeFormString($_POST["lastName"]);
+$email = FormSanitizer::sanitizeFormEmail($_POST["email"]);
+
+if ($account->updateDetails($firstName, $lastName, $email, $username)) {
+  echo "
     <div class='alert alert-success' role='alert'>
       Account details updated.
     </div>
     ";
-  } else {
-    $errorMessage = $account->getFirstError();
+} else {
+  $errorMessage = $account->getFirstError();
 
-    echo "
+  echo "
     <div class='alert alert-danger' role='alert'>
       $errorMessage
     </div>
     ";
-  }
 }
