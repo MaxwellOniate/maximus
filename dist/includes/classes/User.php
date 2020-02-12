75d98d64e0cc07e34ee3d08b05b333671bdc6549
maxwelloniate <maxwelloniate@gmail.com>
2020-02-11 22:08:11 -0500

    User get functions.

--- dist/includes/classes/User.php
+++ dist/includes/classes/User.php
@@ -4,29 +4,33 @@ class User
 {
   private $con;
   private $username;
+  private $sqlData;
 
   public function __construct($con, $username)
   {
     $this->con = $con;
     $this->username = $username;
+
+    $query = $con->prepare("SELECT * FROM users WHERE username = :username");
+    $query->execute([':username' => $username]);
+
+    $this->sqlData = $query->fetch(PDO::FETCH_ASSOC);
   }
 
   public function getUsername()
   {
     return $this->username;
   }
+  public function getFirstName()
+  {
+    return $this->sqlData['firstName'];
+  }
+  public function getLastName()
+  {
+    return $this->sqlData['lastName'];
+  }
   public function getEmail()
   {
-    $query = $this->con->prepare("SELECT email FROM users WHERE username = :username");
-    $query->execute([':username' => $this->username]);
-    $row = $query->fetch(PDO::FETCH_ASSOC);
-    return $row['email'];
-  }
-  public function getFirstAndLastName()
-  {
-    $query = $this->con->prepare("SELECT CONCAT(firstName, ' ', lastName) as 'name' FROM users WHERE username = :username");
-    $query->execute([':username' => $this->username]);
-    $row = $query->fetch(PDO::FETCH_ASSOC);
-    return $row['name'];
+    return $this->sqlData['email'];
   }
 }
