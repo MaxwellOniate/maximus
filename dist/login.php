2346cc4f10d459094b76fd8b3cd48389c5981462
maxwelloniate <maxwelloniate@gmail.com>
2020-02-12 20:52:33 -0500

    Favicon added

--- dist/login.php
+++ dist/login.php
@@ -33,6 +33,7 @@ if (isset($_POST['submit'])) {
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Maximus | Register</title>
+  <link rel="shortcut icon" href="assets/img/favicon.ico" type="image/x-icon">
   <link rel="stylesheet" href="assets/css/bootstrap.css">
   <link rel="stylesheet" href="assets/css/main.css">
 </head>
