642e1a262c53d5a972286e4819139174dcb8db07
maxwelloniate <maxwelloniate@gmail.com>
2020-02-12 20:58:19 -0500

    Title tag fix

--- dist/login.php
+++ dist/login.php
@@ -32,7 +32,7 @@ if (isset($_POST['submit'])) {
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Maximus | Register</title>
+  <title>Maximus | Login</title>
   <link rel="shortcut icon" href="assets/img/favicon.ico" type="image/x-icon">
   <link rel="stylesheet" href="assets/css/bootstrap.css">
   <link rel="stylesheet" href="assets/css/main.css">
